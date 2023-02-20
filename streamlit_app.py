b7aa67de8565a1f0f10617c323bf50548fadb3bf
Kaustubh Kalyankar <kaustubhsanjayk@gmail.com>
2023-02-20 15:49:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,4 @@ if streamlit.button('Get Fruit Load List'):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
 
-import requests
-fruityvice_response_new = requests.get("https://fruityvice.com/api/fruit/"+add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
